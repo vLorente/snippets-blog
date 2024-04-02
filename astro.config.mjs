a272cd33f3bc6831f800f5cbc162d5f96956d5f2
vLorente <valenlj7@gmail.com>
2024-04-02 14:17:16 +0200

    React config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
-import react from "@astrojs/react"
-import tailwind from "@astrojs/tailwind"
-import { defineConfig } from "astro/config"
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
+
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react(), tailwind()]
-})
+  integrations: [tailwind(), react()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    }
+  })
+});
