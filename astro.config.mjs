d25af1c1f4562fe13141e06ff561c1fe854edb74
vLorente <valenlj7@gmail.com>
2024-04-17 00:43:39 +0200

    Add getStaticPaths for page and slug

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import { defineConfig } from "astro/config"
 // https://astro.build/config
 export default defineConfig({
 	integrations: [react(), tailwind()],
-	output: "server",
+	output: "hybrid",
 	adapter: vercel({
 		webAnalytics: {
 			enabled: true
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,2 +1,2 @@
 export const UNCATEGORIZED = "__uncategorized__"
-export const PAGE_SIZE = 8
+export const PAGE_SIZE = 6
