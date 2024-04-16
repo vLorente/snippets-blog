2ba72e90a9d5ab73538438dcb673a45b14044759
vLorente <valenlj7@gmail.com>
2024-04-17 00:09:03 +0200

    Improve PostCard content

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,14 +2,19 @@ import { defineCollection, z } from "astro:content"
 
 const postsCollection = defineCollection({
 	type: "content",
-	schema: z.object({
-		title: z.string(),
-		date: z.date(),
-		description: z.string().optional(),
-		image: z.string().optional(),
-		tags: z.array(z.string()).optional(),
-		category: z.string().optional(),
-	}),
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			date: z.date(),
+			description: z.string().optional(),
+			image: image()
+				.refine((img) => img.width >= 1080, {
+					message: "Cover image must be at least 1080 pixels wide!",
+				})
+				.optional(),
+			tags: z.array(z.string()).optional(),
+			category: z.string().optional(),
+		}),
 })
 export const collections = {
 	posts: postsCollection,
