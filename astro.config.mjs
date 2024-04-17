41389bd3d86b1e1337b30df9208611f35a1788dc
vLorente <valenlj7@gmail.com>
2024-04-17 13:46:22 +0200

    Add remark and rehype

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,18 @@ import react from "@astrojs/react"
 import tailwind from "@astrojs/tailwind"
 import vercel from "@astrojs/vercel/serverless"
 import { defineConfig } from "astro/config"
+import rehypeKatex from "rehype-katex"
+import remarkMath from "remark-math"
+import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs"
 
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [react(), tailwind()],
+	markdown: {
+		remarkPlugins: [remarkMath, remarkReadingTime],
+		rehypePlugins: [rehypeKatex]
+	},
 	output: "hybrid",
 	adapter: vercel({
 		webAnalytics: {
