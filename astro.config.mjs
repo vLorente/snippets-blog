5b10ffe55972adab9e506a8b01a729081bafb0b9
vLorente <valenlj7@gmail.com>
2024-04-19 12:31:08 +0200

    Remove react and vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,4 @@
-import react from "@astrojs/react"
 import tailwind from "@astrojs/tailwind"
-import vercel from "@astrojs/vercel/serverless"
 import swup from "@swup/astro"
 import { defineConfig } from "astro/config"
 import rehypeKatex from "rehype-katex"
@@ -11,7 +9,6 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs"
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
-		react(),
 		tailwind(),
 		swup({
 			theme: false,
@@ -29,9 +26,4 @@ export default defineConfig({
 		rehypePlugins: [rehypeKatex]
 	},
 	output: "hybrid",
-	adapter: vercel({
-		webAnalytics: {
-			enabled: true
-		}
-	})
 })
