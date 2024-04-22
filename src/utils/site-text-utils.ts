9d60f63eb37533c453514c20de31b45d50759830
vLorente <valenlj7@gmail.com>
2024-04-22 18:56:58 +0200

    Some Fixes
    - post static page dont print draft posts
    - getText funtions error

--- src/utils/site-text-utils.ts
+++ src/utils/site-text-utils.ts
@@ -1,13 +1,13 @@
 import { SiteText } from "@constants/site-texts"
 
 export const getMinuteText = (minutes: number): string => {
-	return minutes > 1 ? SiteText.minutesCount : SiteText.minuteCount
+	return minutes !== 1 ? SiteText.minutesCount : SiteText.minuteCount
 }
 
 export const getPostText = (posts: number): string => {
-	return posts > 1 ? SiteText.postsCounts : SiteText.postCount
+	return posts !== 1 ? SiteText.postsCounts : SiteText.postCount
 }
 
 export const getWordText = (words: number): string => {
-	return words > 1 ? SiteText.wordsCount : SiteText.wordCount
+	return words !== 1 ? SiteText.wordsCount : SiteText.wordCount
 }
