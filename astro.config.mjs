45b8fa329f4880e8b302a493ca04ae1ffbf5d7cc
vLorente <valenlj7@gmail.com>
2024-04-22 23:06:23 +0200

    Add vercel deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,25 +5,24 @@ import rehypeKatex from "rehype-katex"
 import remarkMath from "remark-math"
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs"
 
+import vercel from "@astrojs/vercel/serverless"
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		tailwind(),
-		swup({
-			theme: false,
-			animationClass: "transition-",
-			containers: ["main"],
-			smoothScrolling: true,
-			cache: true,
-			preload: true,
-			accessibility: true,
-			globalInstance: true,
-		})
-	],
+	integrations: [tailwind(), swup({
+		theme: false,
+		animationClass: "transition-",
+		containers: ["main"],
+		smoothScrolling: true,
+		cache: true,
+		preload: true,
+		accessibility: true,
+		globalInstance: true
+	})],
 	markdown: {
 		remarkPlugins: [remarkMath, remarkReadingTime],
 		rehypePlugins: [rehypeKatex]
 	},
 	output: "hybrid",
+	adapter: vercel()
 })
